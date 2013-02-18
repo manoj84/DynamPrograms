237c5fa7c15bc1483a098b48eee92f7e77433a05
Manoj <manoj.op@hotmail.com>
2013-02-17 20:10:48 -0500

    Commit

--- src/CoinProblem.java
+++ src/CoinProblem.java
@@ -1,6 +1,3 @@
-
-import sun.jdbc.odbc.JdbcOdbcInputStream;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,6 +8,11 @@ import java.util.List;
  * Date: 1/17/13
  * Time: 10:50 PM
  * To change this template use File | Settings | File Templates.
+ *  Explanation -  Coin problem lets you know if the given
+      coins can be used to form a particular sum or not.
+      Here coins are not allowed to repeat. So, once
+      you picked up a coin, you are not allowed to
+      pick an other one for a given sum.
  */
 
 public class CoinProblem {
@@ -18,38 +20,51 @@ public class CoinProblem {
     public static void main(String args[]) {
         int[] coins = {1, 3, 5};
         int sum = 11;
-        optimize(coins, sum);
+        Coins[] solutions = optimize(coins, sum);
+        for(int i=0; i<solutions.length; i++){
+            System.out.println(" For Sum " + i + "  Minimum Coins are  " + solutions[i].getSum() + "" +
+                    "  Values are " + solutions[i].getValues().toString());
+        }
     }
 
-    public static void optimize(int[] coins, int sum) {
-        Coins[] [] data = new Coins[coins.length][sum];
+    public static Coins[] optimize(int[] coins, int sum) {
+        Coins[][] data = new Coins[sum][coins.length];
         Coins[] optimized = new Coins[sum];
-        for (int i =0; i<sum; i++){
+        for (int i = 0; i < sum; i++) {
             int temp = 100;
-            for (int j=0; j<coins.length; j++){
+            Coins min = null;
+            for (int j = 0; j < coins.length; j++) {
                 List values = new ArrayList();
                 int diff = i - coins[j];
                 if (diff == 0) {
                     values.add(j);
                     data[i][j] = new Coins(1, values);
+                } else if (diff > 0) {
+                    List tempvalues = optimized[diff].getValues();
+                    int tempsum = optimized[diff].getSum();
+                    if (tempvalues.contains(j) || tempsum ==0) {
+                        //The coin has already been used
+                        //Mark the sum as zero
+                        data[i][j] = new Coins(0, values);
+                    } else {
+                        //The coin is not there
+                        tempvalues.add(j);
+                        data[i][j] = new Coins(tempsum + 1, tempvalues);
+                    }
+                } else if (diff < 0) {
+                    data[i][j] = new Coins(0, values);
                 }
-                else if (diff > 0) {
-                      List values = optimized[diff].getValues();
-                      if (values.contains(j)){
-                          //The coin has already been used
-                          //Mark the sum as zero
-                      }
 
+                if (data[i][j].getSum() < temp && data[i][j].getSum()>0 ) {
+                        temp = data[i][j].getSum();
+                        optimized[i] = data[i][j];
+                    }
+                else if(temp == 100){
+                    optimized[i] = data[i][j];
                 }
-                else if (diff <0){
-
-                }
-
-                if(data[i][j]<temp && data[i][j]>0) temp = data[i][j];
             }
-            if(temp !=100) optimized[i]  = temp;
         }
-        System.out.println(data);
+        return optimized;
     }
-
 }
+
