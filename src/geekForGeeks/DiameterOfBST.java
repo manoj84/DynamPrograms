bb7946a95d940c8b9f7ee441b1eea9e715111968
Manoj <Manoj@192.168.1.24>
2015-03-27 09:03:32 -0400

    Tree Problems

---
+++ src/geekForGeeks/DiameterOfBST.java
@@ -0,0 +1,23 @@
+package geekForGeeks;
+
+import tree.core.BSTNode;
+import trees.execute.TreeUtil;
+
+public class DiameterOfBST {
+
+    public static void main(String args[]) {
+
+        BSTNode<Integer, String> node = TreeUtil.getTwoWayTree();
+        System.out.println(calculateHeight(node));
+    }
+
+    private static int calculateHeight(BSTNode<Integer, String> node) {
+        // TODO Auto-generated method stub
+        if (node == null) {
+            return 0;
+        }
+
+        return (1 + Math.max(calculateHeight(node.left()),
+                calculateHeight(node.right())));
+    }
+}
--- trees/trees/execute/TreeUtil.java
+++ trees/trees/execute/TreeUtil.java
@@ -4,10 +4,10 @@ package trees.execute;
  *  ==========================================
  *            Binary Tree
  *      =================================   
- *                   6
- *             2        10
- *         1      4        12
- *             3     5   11
+ *                     6 node1
+ *             4 node2        10 node3
+ *        2 node4      5 node5        12 node6
+ * 1 node7     3 node8             11 node9   13 node10
  *   ======================================
  *   Check wiki pedia for in order pre order tree traversal for diagram
  * 
@@ -20,55 +20,55 @@ public final class TreeUtil {
 
     public static BSTNode<Integer, String> getBinarySearchTree() {
 
-        BSTNode<Integer, String> root = new BSTNode<Integer, String>();
-        root.setKey(6);
-        root.setElement("F");
-        BSTNode<Integer, String> left = new BSTNode<Integer, String>();
-        left.setKey(2);
-        left.setElement("B");
-        root.setLeft(left);
-        BSTNode<Integer, String> right = new BSTNode<Integer, String>();
-        right.setKey(7);
-        right.setElement("G");
-        root.setRight(right);
+        BSTNode<Integer, String> node1 = new BSTNode<Integer, String>();
+        node1.setKey(6);
+        node1.setElement("F");
+        BSTNode<Integer, String> node2 = new BSTNode<Integer, String>();
+        node2.setKey(4);
+        node2.setElement("B");
+        node1.setLeft(node2);
 
-        BSTNode<Integer, String> rootLeft = root.left();
-        BSTNode<Integer, String> leftFirst = new BSTNode<Integer, String>();
-        leftFirst.setKey(1);
-        leftFirst.setElement("A");
-        rootLeft.setLeft(leftFirst);
-        BSTNode<Integer, String> rightFirst = new BSTNode<Integer, String>();
-        rightFirst.setKey(4);
-        rightFirst.setElement("D");
-        rootLeft.setRight(rightFirst);
+        BSTNode<Integer, String> node3 = new BSTNode<Integer, String>();
+        node3.setKey(10);
+        node3.setElement("G");
+        node1.setRight(node3);
 
-        BSTNode<Integer, String> rootLeftRight = rootLeft.right();
+        BSTNode<Integer, String> node4 = new BSTNode<Integer, String>();
+        node4.setKey(2);
+        node4.setElement("A");
+        node2.setLeft(node4);
 
-        BSTNode<Integer, String> leftThird = new BSTNode<Integer, String>();
-        leftThird.setKey(3);
-        leftThird.setElement("C");
-        rootLeftRight.setLeft(leftThird);
+        BSTNode<Integer, String> node5 = new BSTNode<Integer, String>();
+        node5.setKey(5);
+        node5.setElement("D");
+        node2.setRight(node5);
 
-        BSTNode<Integer, String> rightThird = new BSTNode<Integer, String>();
-        rightThird.setKey(5);
-        rightThird.setElement("E");
-        rootLeftRight.setRight(rightThird);
+        BSTNode<Integer, String> node7 = new BSTNode<Integer, String>();
+        node7.setKey(1);
+        node7.setElement("C");
+        node4.setLeft(node7);
 
-        BSTNode<Integer, String> rootRight = root.right();
+        BSTNode<Integer, String> node8 = new BSTNode<Integer, String>();
+        node8.setKey(3);
+        node8.setElement("C");
+        node4.setRight(node8);
 
-        BSTNode<Integer, String> rightSecond = new BSTNode<Integer, String>();
-        rightSecond.setKey(12);
-        rightSecond.setElement("I");
-        right.setRight(rightSecond);
+        BSTNode<Integer, String> node6 = new BSTNode<Integer, String>();
+        node6.setKey(12);
+        node6.setElement("I");
+        node3.setRight(node6);
 
-        BSTNode<Integer, String> rootRightRight = rootRight.right();
+        BSTNode<Integer, String> node9 = new BSTNode<Integer, String>();
+        node9.setKey(11);
+        node9.setElement("E");
+        node6.setLeft(node9);
 
-        BSTNode<Integer, String> leftFourth = new BSTNode<Integer, String>();
-        leftFourth.setKey(10);
-        leftFourth.setElement("H");
-        rightSecond.setLeft(leftFourth);
+        BSTNode<Integer, String> node10 = new BSTNode<Integer, String>();
+        node10.setKey(13);
+        node10.setElement("H");
+        node6.setRight(node10);
 
-        return root;
+        return node1;
 
     }
 
@@ -90,6 +90,85 @@ public final class TreeUtil {
 
         return root;
 
+    }
+
+    public static BSTNode<Integer, String> getTwoWayTree() {
+
+        BSTNode<Integer, String> node1 = new BSTNode<Integer, String>();
+        node1.setKey(6);
+        node1.setElement("F");
+
+        BSTNode<Integer, String> node2 = new BSTNode<Integer, String>();
+        node2.setKey(4);
+        node2.setElement("B");
+        node1.setLeft(node2);
+        node2.setParent(node1);
+
+        BSTNode<Integer, String> node3 = new BSTNode<Integer, String>();
+        node3.setKey(10);
+        node3.setElement("G");
+        node1.setRight(node3);
+        node3.setParent(node1);
+
+        BSTNode<Integer, String> node4 = new BSTNode<Integer, String>();
+        node4.setKey(2);
+        node4.setElement("A");
+        node2.setLeft(node4);
+        node4.setParent(node2);
+
+        BSTNode<Integer, String> node5 = new BSTNode<Integer, String>();
+        node5.setParent(node2);
+        node5.setKey(5);
+        node5.setElement("D");
+        node2.setRight(node5);
+
+        BSTNode<Integer, String> node7 = new BSTNode<Integer, String>();
+        node7.setKey(1);
+        node7.setElement("C");
+        node4.setLeft(node7);
+        node7.setParent(node4);
+
+        BSTNode<Integer, String> node8 = new BSTNode<Integer, String>();
+        node8.setKey(3);
+        node8.setElement("C");
+        node4.setRight(node8);
+        node8.setParent(node4);
+
+        BSTNode<Integer, String> node6 = new BSTNode<Integer, String>();
+        node6.setKey(12);
+        node6.setElement("I");
+        node3.setRight(node6);
+        node6.setParent(node3);
+
+        BSTNode<Integer, String> node9 = new BSTNode<Integer, String>();
+        node9.setKey(11);
+        node9.setElement("E");
+        node6.setLeft(node9);
+        node9.setParent(node6);
+
+        BSTNode<Integer, String> node10 = new BSTNode<Integer, String>();
+        node10.setKey(13);
+        node10.setElement("H");
+        node6.setRight(node10);
+        node10.setParent(node6);
+
+        return node1;
+
+    }
+
+    public static void inOrder(BSTNode<Integer, String> node) {
+        if (node == null) {
+            return;
+        }
+
+        inOrder(node.left());
+        System.out.print(node.key() + " ");
+        inOrder(node.right());
+    }
+
+    public static void main(String args[]) {
+        BSTNode<Integer, String> node = getBinarySearchTree();
+        inOrder(node);
     }
 
 }
