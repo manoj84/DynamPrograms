3371333e9eeddb3f5a068b168181779119e15949
Manoj Mandam <mmandam@akamail.com>
2015-08-24 17:47:42 -0400

    LCA of a Binary Tree.

--- src/main/code/trees/trees/execute/LCABinaryNodes.java
+++ src/main/code/trees/trees/execute/LCABinaryNodes.java
@@ -17,15 +17,39 @@ public class LCABinaryNodes {
         LCABinaryNodes lcbnBinaryNodes = new LCABinaryNodes();
         BSTNode<Integer, String> tree = TreeUtil.getBinarySearchTree();
 
-        BSTNode<Integer, String> node1 = new BSTNode<Integer, String>(4, "");
-        BSTNode<Integer, String> node2 = new BSTNode<Integer, String>(12, "");
+        BSTNode<Integer, String> node1 = new BSTNode<Integer, String>(6, "");
+        BSTNode<Integer, String> node2 = new BSTNode<Integer, String>(13, "");
 
-        lcbnBinaryNodes.findLCA(tree, node1, node2);
+        System.out.println(lcbnBinaryNodes.findLCA(tree, node1, node2));
 
     }
 
-    private void findLCA(BSTNode<Integer, String> root,
+    private BSTNode<Integer, String> findLCA(BSTNode<Integer, String> root,
             BSTNode<Integer, String> node1, BSTNode<Integer, String> node2) {
+        if(root == null ) return null;
+        if(root.key() == node1.key() || root.key() == node2.key()) {
+            //Root is the LCA
+            return root;
+        }
+
+        BSTNode<Integer, String> nodeLeft = findLCA(root.left(), node1, node2);
+        BSTNode<Integer, String> nodeRight = findLCA(root.right(), node1, node2);
+
+        if(nodeLeft != null && nodeRight != null) {
+            return root;
+        }
+
+        else if(nodeLeft == null && nodeRight != null) {
+            return nodeRight;
+        }
+
+        else if(nodeRight == null && nodeLeft != null){
+            return nodeLeft;
+        }
+
+        else {
+            return null;
+        }
 
     }
 
