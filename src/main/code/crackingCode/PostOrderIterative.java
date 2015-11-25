c6aab5a74c044ec27ccd7729a54fc348e6d4cdf5
manoj84 <manoj.op@hotmail.com>
2015-11-24 23:18:19 -0500

    Trees Stuff

--- src/main/code/crackingCode/PostOrderIterative.java
+++ src/main/code/crackingCode/PostOrderIterative.java
@@ -1,22 +1,20 @@
 package code.crackingCode;
 
-import code.depthFirstSearch.Node;
 import code.trees.tree.core.BSTNode;
 import code.trees.trees.execute.TreeUtil;
-import com.sun.prism.impl.ps.TessRoundRectRep;
-import javafx.geometry.Pos;
 
-import java.util.*;
-import java.util.concurrent.TransferQueue;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Stack;
 
 /**
  * Created by mmandam on 9/18/15.
  */
 public class PostOrderIterative {
 
-    private BSTNode<Integer, String> node = null;
     Stack<BSTNode> stack = new Stack<BSTNode>();
     Set<String> set = new HashSet<String>();
+    private BSTNode<Integer, String> node = null;
 
     public static void main (String args[]) {
         BSTNode<Integer, String> node = TreeUtil.getBinarySearchTree();
